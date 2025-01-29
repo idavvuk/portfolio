3e3fa4e41d730511381e7210bbff361ca29a9fa0
idavvuk <ida.vranvuk@hotmail.com>
2025-01-29 21:28:58 +0100

    remade the whole thing

--- 2025/src/components/Navbar.jsx
+++ 2025/src/components/Navbar.jsx
@@ -1,23 +1,13 @@
-import { NavLink } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link } from'react-router-dom'
+
+import { styles } from '../styles';
+import { navLinks } from '../constants'; 
+import { logo, menu, close } from '../assets';
+
 const Navbar = () => {
   return (
-    <header className="header"> 
-        <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center 
-                                    justify-center flex font-bold shadow-md">
-            <p className="bg-gradient-to-r from-blue-500 to-blue-800 bg-clip-text text-transparent">IV</p>
-        </NavLink>
-        <nav className="flex text-lg gap-7 font-medium">
-            <NavLink to="/about" className={({isActive}) => isActive ? 
-                'text-blue-500' : 'text-black'}>
-                About
-            </NavLink>
-            <NavLink to="/project" className={({isActive}) => isActive ? 
-                'text-blue-500' : 'text-black'}>
-                Projects
-            </NavLink>
-        
-        </nav>
-    </header>
+    <div>Navbar</div>
   )
 }
 
---
+++ 2025/src/components/Projects.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>Works</div>
+  )
+}
+
+export default Projects
