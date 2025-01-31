f73d0c2661168e5f409561069df6dc18353afe9d
idavvuk <ida.vranvuk@hotmail.com>
2025-01-31 14:44:54 +0100

    added model successfully (finally)

--- 2025/src/components/canvas/Computers.jsx
+++ 2025/src/components/canvas/Computers.jsx
@@ -14,6 +14,10 @@ const Computers = () => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+
       />  
     </mesh>
   )
@@ -45,5 +49,5 @@ const ComputersCanvas = () => {
 
 
 
-export default Computers
+export default ComputersCanvas;
 
