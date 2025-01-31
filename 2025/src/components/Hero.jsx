7cf7ad687dc815df869c897004ef5e075bd4a70c
idavvuk <ida.vranvuk@hotmail.com>
2025-01-31 14:36:08 +0100

    pain

--- 2025/src/components/Hero.jsx
+++ 2025/src/components/Hero.jsx
@@ -6,17 +6,13 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto bg-hero-pattern bg-cover bg-[url('assets/herobg.png')]">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-        {/* Column for circle and line */}
         <div className="flex flex-col justify-center items-center mr-5">
           <div className="flex flex-col justify-center items-center">
-            {/* Circle */}
             <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-            {/* Line */}
             <div className="w-1 sm:h-80 h-40 violet-gradient" />
           </div>
         </div>
 
-        {/* Heading next to the circle */}
         <div className="flex flex-col justify-center items-start">
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I am <span className="text-[#915eff]">Ida
